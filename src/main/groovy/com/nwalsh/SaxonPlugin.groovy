d8ef510dcc0c67f887d2dc18cb00a70465042bb9
Norman Walsh <ndw@nwalsh.com>
2021-05-08 17:02:35 +0100

    Implement global configuration

--- src/main/groovy/com/nwalsh/SaxonPlugin.groovy
+++ src/main/groovy/com/nwalsh/SaxonPlugin.groovy
@@ -3,11 +3,18 @@ package com.nwalsh
 import org.gradle.api.Project
 import org.gradle.api.Plugin
 
+abstract class SaxonPluginExtension {
+    void configure(Closure cl) {
+        cl.delegate = SaxonPluginConfiguration.instance
+        cl()
+    }
+}
 class SaxonPlugin implements Plugin<Project> {
     final String XSLT = 'xslt'
 
     @Override
     void apply(Project project) {
         project.task(XSLT, type: SaxonXsltTask)
+        project.extensions.create("saxon", SaxonPluginExtension)
     }
 }
