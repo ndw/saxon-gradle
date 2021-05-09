23136eb056f526f946c495a2891ce57c9ce24967
Norman Walsh <ndw@nwalsh.com>
2021-05-09 09:41:03 +0100

    Added support for named plugin configurations

--- src/main/groovy/com/nwalsh/SaxonPlugin.groovy
+++ src/main/groovy/com/nwalsh/SaxonPlugin.groovy
@@ -5,16 +5,21 @@ import org.gradle.api.Plugin
 
 abstract class SaxonPluginExtension {
     void configure(Closure cl) {
-        cl.delegate = SaxonPluginConfiguration.instance
+        this.configure(SaxonPluginConfigurations.DEFAULT, cl)
+    }
+    
+    void configure(String name, Closure cl) {
+        cl.delegate = new SaxonPluginConfiguration(name)
         cl()
     }
 }
 class SaxonPlugin implements Plugin<Project> {
     final String XSLT = 'xslt'
+    final String SAXON = 'saxon'
 
     @Override
     void apply(Project project) {
+        project.extensions.create(SAXON, SaxonPluginExtension)
         project.task(XSLT, type: SaxonXsltTask)
-        project.extensions.create("saxon", SaxonPluginExtension)
     }
 }
